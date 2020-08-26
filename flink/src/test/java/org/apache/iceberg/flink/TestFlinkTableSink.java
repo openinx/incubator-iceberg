fb0371c7215e5615eabcad6d73782d305a6d27a2
huzheng <openinx@gmail.com>
2020-08-26 19:45:12 +0800

    Add batch unit tests.

--- flink/src/test/java/org/apache/iceberg/flink/TestFlinkTableSink.java
+++ flink/src/test/java/org/apache/iceberg/flink/TestFlinkTableSink.java
@@ -33,6 +33,7 @@ import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.util.FiniteTestSource;
 import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.TableEnvironment;
 import org.apache.flink.table.api.TableResult;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import org.apache.flink.table.api.config.TableConfigOptions;
@@ -45,11 +46,13 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.iceberg.FileFormat;
 import org.apache.iceberg.PartitionSpec;
 import org.apache.iceberg.TableProperties;
+import org.apache.iceberg.catalog.Catalog;
 import org.apache.iceberg.catalog.TableIdentifier;
 import org.apache.iceberg.data.Record;
 import org.apache.iceberg.hadoop.HadoopCatalog;
 import org.apache.iceberg.relocated.com.google.common.collect.ImmutableMap;
 import org.apache.iceberg.relocated.com.google.common.collect.Lists;
+import org.apache.iceberg.util.Pair;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
@@ -70,27 +73,30 @@ public class TestFlinkTableSink extends AbstractTestBase {
 
   @Rule
   public TemporaryFolder tempFolder = new TemporaryFolder();
-  private String warehouse;
   private String tablePath;
-  private Runnable catalogTableCreator;
+  private String warehouse;
+  private Map<String, String> properties;
+  private Catalog catalog;
+  private StreamExecutionEnvironment env;
+  private StreamTableEnvironment tEnv;
 
   private final FileFormat format;
-  private final boolean useOldPlanner;
   private final int parallelism;
 
-  @Parameterized.Parameters(name = "{index}: format={0}, useOldPlanner={1}, parallelism={2}")
+  @Parameterized.Parameters(name = "{index}: format={0}, parallelism={2}")
   public static Iterable<Object[]> data() {
     return Arrays.asList(
-        new Object[] {"avro", true, 1},
-        new Object[] {"avro", false, 1},
-        new Object[] {"avro", true, 2},
-        new Object[] {"avro", false, 2}
+        new Object[] {"avro", 1},
+        new Object[] {"avro", 2},
+        new Object[] {"orc", 1},
+        new Object[] {"orc", 2},
+        new Object[] {"parquet", 1},
+        new Object[] {"parquet", 2}
     );
   }
 
-  public TestFlinkTableSink(String format, boolean useOldPlanner, int parallelism) {
+  public TestFlinkTableSink(String format, int parallelism) {
     this.format = FileFormat.valueOf(format.toUpperCase(Locale.ENGLISH));
-    this.useOldPlanner = useOldPlanner;
     this.parallelism = parallelism;
   }
 
@@ -102,63 +108,99 @@ public class TestFlinkTableSink extends AbstractTestBase {
     tablePath = warehouse.concat("/default/").concat(TABLE_NAME);
     Assert.assertTrue("Should create the table path correctly.", new File(tablePath).mkdirs());
 
-    Map<String, String> props = ImmutableMap.of(TableProperties.DEFAULT_FILE_FORMAT, format.name());
-    HadoopCatalog catalog = new HadoopCatalog(CONF, warehouse);
-    catalogTableCreator = () -> catalog
-        .createTable(TableIdentifier.parse("default." + TABLE_NAME),
-            SimpleDataUtil.SCHEMA,
-            PartitionSpec.unpartitioned(),
-            props);
-  }
+    properties = ImmutableMap.of(TableProperties.DEFAULT_FILE_FORMAT, format.name());
+    catalog = new HadoopCatalog(CONF, warehouse);
 
-  @Test
-  public void testSQL() throws Exception {
-    StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-
+    env = StreamExecutionEnvironment.getExecutionEnvironment();
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
     env.enableCheckpointing(400);
     env.setParallelism(parallelism);
 
-    EnvironmentSettings.Builder settingsBuilder = EnvironmentSettings.newInstance().inStreamingMode();
-    if (useOldPlanner) {
-      settingsBuilder.useBlinkPlanner();
-    }
-    StreamTableEnvironment tEnv = StreamTableEnvironment.create(env, settingsBuilder.build());
+    EnvironmentSettings settings = EnvironmentSettings
+        .newInstance()
+        .useBlinkPlanner()
+        .inStreamingMode()
+        .build();
+    tEnv = StreamTableEnvironment.create(env, settings);
     tEnv.executeSql(String.format("create catalog iceberg_catalog with (" +
         "'type'='iceberg', 'catalog-type'='hadoop', 'warehouse'='%s')", warehouse));
     tEnv.executeSql("use catalog iceberg_catalog");
     tEnv.getConfig().getConfiguration().set(TableConfigOptions.TABLE_DYNAMIC_TABLE_OPTIONS_ENABLED, true);
 
-    // Create the table.
-    catalogTableCreator.run();
+    catalog.createTable(TableIdentifier.parse("default." + TABLE_NAME),
+        SimpleDataUtil.SCHEMA,
+        PartitionSpec.unpartitioned(),
+        properties);
+  }
 
+  private DataStream<RowData> generateInputStream(List<Row> rows) {
+    TypeInformation<Row> typeInformation = new RowTypeInfo(SimpleDataUtil.FLINK_SCHEMA.getFieldTypes());
+    return env.addSource(new FiniteTestSource<>(rows), typeInformation)
+        .map(CONVERTER::toInternal, RowDataTypeInfo.of(SimpleDataUtil.ROW_TYPE));
+  }
+
+  private Pair<List<Row>, List<Record>> generateData() {
     String[] worlds = new String[] {"hello", "world", "foo", "bar", "apache", "foundation"};
     List<Row> rows = Lists.newArrayList();
+    List<Record> expected = Lists.newArrayList();
     for (int i = 0; i < worlds.length; i++) {
       rows.add(Row.of(i + 1, worlds[i]));
+      Record record = SimpleDataUtil.createRecord(i + 1, worlds[i]);
+      expected.add(record);
+      expected.add(record);
     }
-    TypeInformation<Row> typeInformation = new RowTypeInfo(SimpleDataUtil.FLINK_SCHEMA.getFieldTypes());
-    DataStream<RowData> stream = env.addSource(new FiniteTestSource<>(rows), typeInformation)
-        .map(CONVERTER::toInternal, RowDataTypeInfo.of(SimpleDataUtil.ROW_TYPE));
+    return Pair.of(rows, expected);
+  }
+
+  @Test
+  public void testStreamSQL() throws Exception {
+    Pair<List<Row>, List<Record>> data = generateData();
+    List<Row> rows = data.first();
+    List<Record> expected = data.second();
+    DataStream<RowData> stream = generateInputStream(rows);
 
     // Register the rows into a temporary table named 'sourceTable'.
     tEnv.createTemporaryView("sourceTable", tEnv.fromDataStream(stream, $("id"), $("data")));
 
     // Redirect the records from source table to destination table.
-    String insertSQL = String.format("INSERT INTO %s SELECT id,data from sourceTable",
-        TABLE_NAME);
+    String insertSQL = String.format("INSERT INTO %s SELECT id,data from sourceTable", TABLE_NAME);
     TableResult result = tEnv.executeSql(insertSQL);
     waitComplete(result);
 
     // Assert the table records as expected.
-    List<Record> expected = Lists.newArrayList();
-    // There will be two checkpoints in FiniteTestSource, so it will produce a list with double records.
-    for (int i = 0; i < 2; i++) {
-      for (int k = 0; k < worlds.length; k++) {
-        expected.add(SimpleDataUtil.createRecord(k + 1, worlds[k]));
-      }
-    }
     SimpleDataUtil.assertTableRecords(tablePath, expected);
+  }
+
+  @Test
+  public void testBatchSQL() throws Exception {
+    EnvironmentSettings settings = EnvironmentSettings
+        .newInstance()
+        .inBatchMode()
+        .useBlinkPlanner()
+        .build();
+    TableEnvironment batchEnv = TableEnvironment.create(settings);
+    batchEnv.executeSql(String.format("create catalog batch_catalog with (" +
+        "'type'='iceberg', 'catalog-type'='hadoop', 'warehouse'='%s')", warehouse));
+    batchEnv.executeSql("use catalog batch_catalog");
+    batchEnv.getConfig().getConfiguration().set(TableConfigOptions.TABLE_DYNAMIC_TABLE_OPTIONS_ENABLED, true);
+
+    // Create source table.
+    catalog.createTable(TableIdentifier.parse("default.sourceTable"),
+        SimpleDataUtil.SCHEMA,
+        PartitionSpec.unpartitioned(),
+        properties);
+
+    TableResult result;
+    String[] words = new String[] {"hello", "world", "apache"};
+    List<Record> expected = Lists.newArrayList();
+    for (int i = 0; i < words.length; i++) {
+      expected.add(SimpleDataUtil.createRecord(i, words[i]));
+      result = batchEnv.executeSql(String.format("INSERT INTO sourceTable SELECT %d, '%s'", i, words[i]));
+      waitComplete(result);
+    }
+
+    // Assert the table records as expected.
+    SimpleDataUtil.assertTableRecords(warehouse.concat("/default/sourceTable"), expected);
   }
 
   private static void waitComplete(TableResult result) {
